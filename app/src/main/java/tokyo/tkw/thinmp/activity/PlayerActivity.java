5bcfe17dd1a6dc4a933ccc371833f7d306b56a56
tkw.tokyo <tkw.tokyo@gmail.com>
2019-03-19 08:57:19 +0900

    プレイヤー　次へボタンのイベント設定

--- app/src/main/java/tokyo/tkw/thinmp/activity/PlayerActivity.java
+++ app/src/main/java/tokyo/tkw/thinmp/activity/PlayerActivity.java
@@ -59,6 +59,7 @@ public class PlayerActivity extends AppCompatActivity {
         @Override
         public void onNext() {
             mMusicService.next();
+            updatePlayer();
         }
 
         @Override
