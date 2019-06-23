2d00e8f1a40873469ff7f13f9dc1f45f839c514b
tkw.tokyo <tkw.tokyo@gmail.com>
2019-06-24 00:43:16 +0900

    画像差し替え。画像を角丸にする。

--- app/src/main/java/tokyo/tkw/thinmp/util/GlideUtil.java
+++ app/src/main/java/tokyo/tkw/thinmp/util/GlideUtil.java
@@ -9,7 +9,10 @@ import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.MultiTransformation;
 import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
@@ -22,8 +25,8 @@ import static android.net.Uri.parse;
 
 public class GlideUtil {
     private static final String ALBUM_ART_URL = "content://media/external/audio/albumart/";
-    public static final int ALBUM_RESOURCE_ID = R.drawable.album;
-    public static final int ARTIST_RESOURCE_ID = R.drawable.artist;
+    public static final int ALBUM_RESOURCE_ID = R.drawable.default_album_art;
+    public static final int ARTIST_RESOURCE_ID = R.drawable.default_artist_art;
     private static final int DEFAULT_RESOURCE_ID = ALBUM_RESOURCE_ID;
 
     public static void bitmap(String albumArtId, ImageView view) {
@@ -65,23 +68,29 @@ public class GlideUtil {
 
         public void exec() {
             if (mIterator.hasNext()) {
-                Glide.with(mView).asBitmap().load(createUri((String) mIterator.next())).listener(new RequestListener<Bitmap>() {
-                    Handler handler = new Handler();
+                Glide.with(mView)
+                        .asBitmap()
+                        .load(createUri((String) mIterator.next()))
+                        .transform(new FitCenter(), new RoundedCorners(10))
+                        .listener(new RequestListener<Bitmap>() {
+                            Handler handler = new Handler();
 
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model,
-                                                Target<Bitmap> target, boolean isFirstResource) {
-                        handler.post(() -> exec());
-                        return false;
-                    }
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model,
+                                                        Target<Bitmap> target,
+                                                        boolean isFirstResource) {
+                                handler.post(() -> exec());
+                                return false;
+                            }
 
-                    @Override
-                    public boolean onResourceReady(Bitmap resource, Object model,
-                                                   Target<Bitmap> target, DataSource dataSource,
-                                                   boolean isFirstResource) {
-                        return false;
-                    }
-                }).into(mView);
+                            @Override
+                            public boolean onResourceReady(Bitmap resource, Object model,
+                                                           Target<Bitmap> target,
+                                                           DataSource dataSource,
+                                                           boolean isFirstResource) {
+                                return false;
+                            }
+                        }).into(mView);
             } else {
                 Glide.with(mView).asBitmap().load(mResourceId).into(mView);
             }
