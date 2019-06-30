383aa9875a3e25dd10a97910af55de8d02a11764
tkw.tokyo <tkw.tokyo@gmail.com>
2019-06-30 19:24:16 +0900

    リファクタ 不要な記述を削除

--- app/src/main/java/tokyo/tkw/thinmp/provider/ArtistContentProvider.java
+++ app/src/main/java/tokyo/tkw/thinmp/provider/ArtistContentProvider.java
@@ -7,12 +7,6 @@ public class ArtistContentProvider extends MediaStoreAudioArtistsProvider {
     public ArtistContentProvider(Context context, String artistId) {
         super(context);
 
-        projection = new String[]{
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Artists.ARTIST,
-                MediaStore.Audio.Artists.NUMBER_OF_ALBUMS,
-                MediaStore.Audio.Artists.NUMBER_OF_TRACKS
-        };
         selection = MediaStore.Audio.Media._ID + " = ?";
         selectionArgs = new String[]{artistId};
         sortOrder = null;
