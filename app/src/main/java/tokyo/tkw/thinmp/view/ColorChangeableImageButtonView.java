0fbf79d0fab52a462efa01dbfabf9f84fb67fe0d
tkw.tokyo <tkw.tokyo@gmail.com>
2019-07-22 22:20:59 +0900

    アイコンの色を分ける

--- app/src/main/java/tokyo/tkw/thinmp/view/ColorChangeableImageButtonView.java
+++ app/src/main/java/tokyo/tkw/thinmp/view/ColorChangeableImageButtonView.java
@@ -1,6 +1,9 @@
 package tokyo.tkw.thinmp.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 
@@ -12,7 +15,15 @@ public class ColorChangeableImageButtonView extends AppCompatImageButton {
     public ColorChangeableImageButtonView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        int color = getContext().getColor(R.color.colorAccent);
-        setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        @SuppressLint({"Recycle", "CustomViewStyleable"}) TypedArray typedArray =
+                context.obtainStyledAttributes(attrs, R.styleable.Icon);
+        String value = typedArray.getString(R.styleable.Icon_iconColor);
+
+        if (value != null) {
+            setColorFilter(Color.parseColor(value), PorterDuff.Mode.SRC_IN);
+        } else {
+            int color = getContext().getColor(R.color.colorIconMain);
+            setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        }
     }
 }
