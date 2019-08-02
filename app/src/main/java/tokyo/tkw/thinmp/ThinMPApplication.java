16746a7b8a6c12f49914e8149f5a89133e7a7391
tkw.tokyo <tkw.tokyo@gmail.com>
2019-08-02 22:07:54 +0900

    リファクタ リネーム Application

--- app/src/main/java/tokyo/tkw/thinmp/ThinMPApplication.java
+++ app/src/main/java/tokyo/tkw/thinmp/ThinMPApplication.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import io.realm.Realm;
 import tokyo.tkw.thinmp.util.GlideUtil;
 
-public class MyApplication extends Application {
+public class ThinMPApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
